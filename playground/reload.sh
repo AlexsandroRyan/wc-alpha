9849918c0e30da467843a707d2b3d4e0dca60f7b
Alexsandro <alexsandrocosta855@gmail.com>
2024-05-23 21:44:58 -0300

    wip: adding optional parameters

---
+++ playground/reload.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd ..
+sudo mvn clean install
+cd target
+cp wc-alpha-1.0-SNAPSHOT.jar ../playground
+cd ../playground 
+java -jar wc-alpha-1.0-SNAPSHOT.jar test.txt
--- src/main/java/org/alex/wc_alpha/WcAlpha.java
+++ src/main/java/org/alex/wc_alpha/WcAlpha.java
@@ -2,6 +2,7 @@ package org.alex.wc_alpha;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.io.File;
@@ -14,15 +15,48 @@ class WcAlpha implements Callable<Integer> {
     @Parameters(index = "0", description = "The file whose wc-alpha to calculate.")
     private File file;
 
+    @Option(names = "-c", description = "Outputs the number of bytes in a file")
+    boolean byte_count;
+
+    @Option(names = "-l", description = "Outputs the number of lines in a file")
+    boolean line_count;
+
+    @Option(names = "-w", description = "Outputs the number of words in a file")
+    boolean word_count;
+
+    @Option(names = "-m", description = "Outputs the number of characters in a file")
+    boolean character_count;
+
     @Override
     public Integer call() throws Exception { // your business logic goes here...
         try (Scanner sc = new Scanner(new FileInputStream(file))) {
-            int count = 0;
-            while (sc.hasNext()) {
-                sc.next();
-                count++;
+            if (byte_count) {
+                System.out.println(file.length());
             }
-            System.out.printf(String.valueOf(count));
+            if (line_count) {
+                int count = 0;
+                while (sc.hasNextLine()) {
+                    sc.nextLine();
+                    count++;
+                }
+                System.out.println(count);
+            }
+            if (word_count) {
+                int count = 0;
+                while (sc.hasNext()) {
+                    sc.next();
+                    count++;
+                }
+                System.out.println(count);
+            }
+            if (character_count) {
+                int count = 0;
+                while (sc.hasNext()) {
+                    sc.next();
+                    count += sc.next().length();
+                }
+                System.out.println(count);
+            }
         }
         return 0;
     }
