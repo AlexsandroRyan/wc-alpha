a3aa78b1b4a489ef950238e791d83d2c57928a22
Alexsandro <alexsandrocosta855@gmail.com>
2024-05-25 16:47:47 -0300

    feat: finished adding opt parameters; added default response

--- src/main/java/org/alex/wc_alpha/WcAlpha.java
+++ src/main/java/org/alex/wc_alpha/WcAlpha.java
@@ -27,35 +27,62 @@ class WcAlpha implements Callable<Integer> {
     @Option(names = "-m", description = "Outputs the number of characters in a file")
     boolean character_count;
 
+    public long countBytes(File file) {
+        return file.length();
+    }
+
+    public long countLines(File file) throws Exception {
+        try (Scanner sc = new Scanner(new FileInputStream(file))) {
+            long count = 0;
+            while (sc.hasNextLine()) {
+                sc.nextLine();
+                count++;
+            }
+            return count;
+        }
+    }
+
+    public long countWords(File file) throws Exception {
+        try (Scanner sc = new Scanner(new FileInputStream(file))) {
+            long count = 0;
+            while (sc.hasNext()) {
+                sc.next();
+                count++;
+            }
+            return count;
+        }
+    }
+
+    public long countCharacters(File file) throws Exception {
+        try (Scanner sc = new Scanner(new FileInputStream(file))) {
+            long count = 0;
+            while (sc.hasNextLine()) {
+                String answer = sc.nextLine();
+                count += answer.length();
+            }
+            sc.close();
+            return count;
+        }
+    }
+
     @Override
     public Integer call() throws Exception { // your business logic goes here...
         try (Scanner sc = new Scanner(new FileInputStream(file))) {
             if (byte_count) {
-                System.out.println(file.length());
+                System.out.println(countBytes(file) + " " + file.getName());
             }
             if (line_count) {
-                int count = 0;
-                while (sc.hasNextLine()) {
-                    sc.nextLine();
-                    count++;
-                }
-                System.out.println(count);
+                System.out.println(countLines(file) + " " + file.getName());
             }
             if (word_count) {
-                int count = 0;
-                while (sc.hasNext()) {
-                    sc.next();
-                    count++;
-                }
-                System.out.println(count);
+                System.out.println(countWords(file) + " " + file.getName());
             }
             if (character_count) {
-                int count = 0;
-                while (sc.hasNext()) {
-                    sc.next();
-                    count += sc.next().length();
-                }
-                System.out.println(count);
+                System.out.println(countCharacters(file) + " " + file.getName());
+            }
+            if (!byte_count && !line_count && !word_count && !character_count) {
+                System.out.println(
+                        countLines(file) + " " + countWords(file) + " " + countCharacters(file) + " " + file.getName());
             }
         }
         return 0;
